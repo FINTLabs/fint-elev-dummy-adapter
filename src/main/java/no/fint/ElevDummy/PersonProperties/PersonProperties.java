04a6b355b638df3eed49a798c28d323bff57b13b
Hknots <hknots99@gmail.com>
2024-11-26 08:53:44 +0100

    turn into record

--- src/main/java/no/fint/ElevDummy/PersonProperties/PersonProperties.java
+++ src/main/java/no/fint/ElevDummy/PersonProperties/PersonProperties.java
@@ -1,15 +1,12 @@
 package no.fint.ElevDummy.PersonProperties;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@Data
-@Configuration
-@ConfigurationProperties("person")
-public class PersonProperties {
-    private NameProperties names;
-    private List<String> birthdate;
+@ConfigurationProperties(value = "person")
+public record PersonProperties(
+        NameProperties names,
+        List<String> birthdate
+) {
 }
