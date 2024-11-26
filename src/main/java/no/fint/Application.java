9d0be7c7be3fa58ac16477528ceb546ccd8c0d75
Hknots <hknots99@gmail.com>
2024-11-26 08:54:15 +0100

    add CofigurationPropertiesScan

--- src/main/java/no/fint/Application.java
+++ src/main/java/no/fint/Application.java
@@ -1,14 +1,13 @@
 package no.fint;
 
-import no.fint.oauth.OAuthConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@Import(OAuthConfig.class)
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Application {
 
     public static void main(String[] args) {
