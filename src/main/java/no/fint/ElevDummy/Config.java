1473267dd114253012b03c23beeb7143ebf7dcaa
Hknots <hknots99@gmail.com>
2024-11-26 08:55:47 +0100

    remove class variable & move dependency injection to PostConstruct parameter

--- src/main/java/no/fint/ElevDummy/Config.java
+++ src/main/java/no/fint/ElevDummy/Config.java
@@ -2,25 +2,22 @@ package no.fint.ElevDummy;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-@Configuration
 @Slf4j
+@Configuration
 public class Config {
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @PostConstruct
-    public void init() {
+    public void init(ObjectMapper objectMapper) {
         objectMapper.setDateFormat(new ISO8601DateFormat());
     }
 
